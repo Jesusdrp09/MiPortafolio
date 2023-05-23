3c410e15d21a7a084ab0648f623efcc695f07d30
Jesus Rios <jesusdrp09@gmail.com>
2023-05-23 03:36:22 -0500

    Completado a falta de verificación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ServiciosService } from './servicios.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MiPortafolio';
+  width = screen.availWidth;
+  height = screen.availHeight*0.902;
+
+  constructor(public servicios: ServiciosService) {}
+
+  estilos(){
+    var url = this.servicios.get();
+    return {'background-image': 'url('+url+')',
+    'width': this.width.valueOf().toString()+'px', 
+    'height': this.height.valueOf().toString()+'px',
+    'background-size': '100% 100%'}
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,53 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ServiciosService } from './servicios.service';
+import { SkillsComponent } from './skills/skills.component';
+import { ExperiencedComponent } from './experienced/experienced.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  {
+    path: 'about me', 
+    component: AboutMeComponent
+  },
+  {
+    path: 'skills',
+    component: SkillsComponent
+  },
+  {
+    path: 'experienced',
+    component: ExperiencedComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    ExperiencedComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(public servicios: ServiciosService) {}
+
+  cambiar(){
+    this.servicios.cambiar();
+  }
+}
